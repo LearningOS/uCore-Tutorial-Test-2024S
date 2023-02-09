05730fc2c34e94b774cb52d05de971cd3c715dc9
holder <rcycyzh@163.com>
2023-02-09 23:22:42 +0800

    Add base test case into usertest base test case into usertest for chapter 5,6,7,8

--- src/ch7b_usertest.c
+++ src/ch7b_usertest.c
@@ -6,10 +6,11 @@
 /// 辅助测例，运行所有其他测例。
 
 const char *TESTS[] = {
-	"ch2b_hello_world\0", "ch2b_power\0",	  
-	"ch3b_sleep\0",	      "ch3b_sleep1\0",	  "ch3b_yield0\0",
-	"ch3b_yield1\0",      "ch3b_yield2\0",	  "ch5b_getpid\0",
-	"ch5b_forktest0\0",   "ch5b_forktest1\0", "ch5b_forktest2\0",
+	"ch2b_hello_world\0", "ch2b_power\0",	  "ch3b_sleep\0",
+	"ch3b_sleep1\0",      "ch3b_yield0\0",	  "ch3b_yield1\0",
+	"ch3b_yield2\0",      "ch5b_getpid\0",	  "ch5b_forktest0\0",
+	"ch5b_forktest1\0",   "ch5b_forktest2\0", "ch5b_exit\0",
+	"ch6b_filetest\0",    "ch6b_exit"         "ch6b_exec\0",
 	"ch7b_pipetest\0",
 };
 
@@ -18,6 +19,6 @@ int main()
 	int num_test = sizeof(TESTS) / sizeof(char *);
 	int succ = run_tests(TESTS, num_test);
 	assert_eq(succ, num_test);
-	puts("ch6b Usertests passed!");
+	puts("ch7b Usertests passed!");
 	return 0;
 }
